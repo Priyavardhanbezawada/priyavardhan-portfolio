cfea812bf9c2590189fe77a8add00dabaddd517d
Bezawada Priyavardhan <priyavardhan.bezawada@gmail.com>
2026-06-18 21:15:07 +0530

    Updates

--- _next/static/5hiK_If_XCB_JfD_d8w_u/_buildManifest.js
+++ _next/static/5hiK_If_XCB_JfD_d8w_u/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/priyavardhan-portfolio//_next/:path+",
+        "destination": "/priyavardhan-portfolio/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
